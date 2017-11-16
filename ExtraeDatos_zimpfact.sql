02ff02631526d3cb53417773ed53c7ea590224f9
Ben Sánchez <bntmunich@hotmail.com>
2017-11-16 09:58:25 -0600

    actualiza querys

--- ExtraeDatos_zimpfact.sql
+++ ExtraeDatos_zimpfact.sql
@@ -2,26 +2,26 @@
 
   select   
      zf.*
-   , d.id_divisa
+  -- , d.id_divisa
    , d.desc_divisa  
-   , fp.id_forma_pago
+  -- , fp.id_forma_pago
    , fp.desc_forma_pago
-   , r.id_rubro
+   --, r.id_rubro
    , r.desc_rubro
-   , cj.id_caja
+   --, cj.id_caja
    , cj.desc_caja
    , case 
 			when zf.estatus= 'I' then 'IMPORTADO'
 			when zf.estatus= 'R' then 'RECHAZADO'
 			else 'N/A'
 	end as Estatus_zf
-  , o.origen_mov
+ -- , o.origen_mov
   , o.desc_origen_mov
-  , fpo.id_forma_pago as id_forma_pago_original
+ -- , fpo.id_forma_pago as id_forma_pago_original
   , fpo.desc_forma_pago as desc_forma_pago_original
-  , COALESCE ( b.id_banco,'0') id_banco_pagador_bancos
+ -- , COALESCE ( b.id_banco,'0') id_banco_pagador_bancos
   , COALESCE (b.desc_banco,'SIN BANCO PAGADOR') as banco_desc_pagador
-  , COALESCE (as4.id_tipo_docto_as400,'0')id_tipo_docto_as400
+ -- , COALESCE (as4.id_tipo_docto_as400,'0')id_tipo_docto_as400
   , COALESCE (as4.desc_tipo_docto_as400,'SIN ID TIPO DOCTO AS400')desc_tipo_docto_as400
   from  sethdzqa..zimp_fact zf
   inner join sethdzqa..cat_divisa  d on  zf.id_divisa=d.id_divisa 
